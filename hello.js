7569d14149ab9151aa8f161f0c6b0fdd7874a8a2
miftajannat <miftajannat.dev@gmail.com>
2022-10-10 12:54:05 +0600

    first commit

---
+++ hello.js
@@ -0,0 +1,3 @@
+var promise = "I promise I will work hard to become a programmer";
+console.log(promise.split("I"));
+
---
+++ variable.js
@@ -0,0 +1,4 @@
+var number1 = 25;
+var number2 = 15.5;
+console.log(number1 + number2);
+
