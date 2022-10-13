d309d8ffc3f4493fa509e89e5b09227ef9d6a9bb
miftajannat <miftajannat.dev@gmail.com>
2022-10-13 21:35:24 +0600

    forloop comment

---
+++ forloop.js
@@ -0,0 +1,11 @@
+for (var i = 0; i < 10; i++) {
+    console.log(i);
+}
+// 0, 1, 2, 3, 4, 5, 6, 7, 8, 9
+
+var num = [45, 65, 78, 12, 3, 54, 65];
+
+for (var i = 0; i < num.length; i++) {
+    var element = num[i];
+    console.log(element);
+}
--- while.js
+++ while.js
@@ -10,7 +10,7 @@
 //     // num = num + 1;
 
 // }
-
+// while loop continuously cholte thake
 var num = 0;
 while (num <= 15) {
     console.log(num);
