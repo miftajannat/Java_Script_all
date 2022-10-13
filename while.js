36e4833667c6dbd4fcba2777b774ff109f76f6dc
miftajannat <miftajannat.dev@gmail.com>
2022-10-13 20:43:14 +0600

    while loop

---
+++ while.js
@@ -0,0 +1,20 @@
+// conditional loop
+// while (condition) {
+//     // code
+// }
+
+// var num = 0;
+// while (num < 15) {
+//     console.log(num);
+//     num++;
+//     // num = num + 1;
+
+// }
+
+var num = 0;
+while (num <= 15) {
+    console.log(num);
+    num++;
+    // num = num + 1;
+
+}
