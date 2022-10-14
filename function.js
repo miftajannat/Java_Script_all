bdf01009e0dd6b4f00c518e0ea355535a103e1b9
miftajannat <miftajannat.dev@gmail.com>
2022-10-14 09:35:33 +0600

    1st commit function

---
+++ function.js
@@ -0,0 +1,20 @@
+function sayLoveYou() {
+    console.log('I love you');
+    console.log('I love you 3000');
+}
+sayLoveYou();
+
+var date = 14;
+var place = "Resturent";
+sayLoveYou();
+var hangoutPlace = "TSC";
+sayLoveYou();
+
+function doubleIt(num) {
+    // parameter
+    var result = num * 2;
+    console.log(result);
+}
+doubleIt(5);
+doubleIt(50);
+doubleIt(500);
