5697a0ff26d139cf50cc26eeb2a88832e8b3494b
miftajannat <miftajannat.dev@gmail.com>
2022-10-14 17:33:14 +0600

    2nd function commit

--- function.js
+++ function.js
@@ -1,20 +1,27 @@
-function sayLoveYou() {
-    console.log('I love you');
-    console.log('I love you 3000');
-}
-sayLoveYou();
+// function sayLoveYou() {
+//     console.log('I love you');
+//     console.log('I love you 3000');
+// }
+// sayLoveYou();
 
-var date = 14;
-var place = "Resturent";
-sayLoveYou();
-var hangoutPlace = "TSC";
-sayLoveYou();
+// var date = 14;
+// var place = "Resturent";
+// sayLoveYou();
+// var hangoutPlace = "TSC";
+// sayLoveYou();
 
 function doubleIt(num) {
-    // parameter
     var result = num * 2;
     console.log(result);
 }
 doubleIt(5);
 doubleIt(50);
 doubleIt(500);
+
+function doubleIt(num) {
+    var results = num * 4;
+    return results;
+}
+var first = doubleIt(5);
+var second = doubleIt(50);
+console.log(first, second);
